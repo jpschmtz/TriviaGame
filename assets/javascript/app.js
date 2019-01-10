de2400490747f196d7e5cef9e49be818050b3bb8
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-01-09 18:27:05 -0600

    majority of the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var qBank = {
         }
     },
     q2:{
-        qText:"Question 1",
+        qText:"Question 2",
         qAns:{
             wrongAns: "wrong",
             correctAns: "correct",
@@ -16,17 +16,60 @@ var qBank = {
     }
 
 }
+
+var clockRunning;
+var random=Math.floor(Math.random() * 2);
 var correctAnswer;
+var time = 5000;
+var windowTimeout;
 
-//  Timeouts in JavaScript
-//  Set our window alert to run one second after the function's called.
-var windowTimeout = setTimeout(function() {
-alert("Times Up!");
-}, 1000);
+function reset() {
+    // clearTimeout(windowTimeout);
+    time = 5000;
+    $("#question").text("" + qBank.q1.qText + "");
+    $("#ans1").attr('value', "" + qBank.q1.qAns.wrongAns[2] + "");
+    $("#ans2").html("" + qBank.q1.qAns.wrongAns + "");
+    $("#ans3").html("" + qBank.q1.qAns.correctAns + "");
+    $("#ans4").html("" + qBank.q1.qAns.wrongAns + "");
+    console.log("Cleared")
+    setInterval(count, 1000);
+    windowTimeout = setTimeout(function() {
+        alert("Times Up!");
+        }, time);
+  }
 
-//  Cancel window alert on click.
-$("#correctAnswer").on("click", function() {
-//  Clear the timeout, and stop the window alert.
-clearTimeout(windowTimeout);
-});
+
+
+function count() {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
+    time=time-1000;
+    console.log(time)
+    // DONE: Get the current time, pass that into the timeConverter function,
+    //       and save the result in a variable.
+    var converted = timeConverter(time);
+    
+    if (time>=0) {
+        console.log(converted);
+        $("#display").text(converted);
+    }
+  }
+  function timeConverter(t) {
+
+    var seconds = Math.floor(t / 1000);
+    var minutes = "0";
+    return "Seconds Remaining " + seconds;
+  }
+
+function ansCheck() {
+
+    console.log(this);
+    alert("pause");
+    reset()
+}
+window.onload = function() {
+
+    reset()
+    $(".buttons").on("click", ansCheck);
+};
 
