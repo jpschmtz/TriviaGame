cd88a837a841308bb3cf09b782787e281d29f74b
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-01-10 18:09:48 -0600

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,14 @@ var qBank = {
     q1:{
         qText:"Question 1",
         qAns:{
-            wrongAns: "wrong",
-            correctAns: "correct",
+            wrongAns:{
+                text: "wrong",
+                value : false
+            },
+            correctAns: {
+                text: "correct",
+                value : true
+            }
         }
     },
     q2:{
@@ -20,17 +26,25 @@ var qBank = {
 var clockRunning;
 var random=Math.floor(Math.random() * 2);
 var correctAnswer;
-var time = 5000;
+var time;
 var windowTimeout;
 
 function reset() {
     // clearTimeout(windowTimeout);
-    time = 5000;
+    time = 10000;
     $("#question").text("" + qBank.q1.qText + "");
-    $("#ans1").attr('value', "" + qBank.q1.qAns.wrongAns[2] + "");
-    $("#ans2").html("" + qBank.q1.qAns.wrongAns + "");
-    $("#ans3").html("" + qBank.q1.qAns.correctAns + "");
-    $("#ans4").html("" + qBank.q1.qAns.wrongAns + "");
+    $("#ans1").html("" + qBank.q1.qAns.wrongAns.text + "");
+    $("#ans1").val("" + qBank.q1.qAns.wrongAns.value + "");
+
+    $("#ans2").html("" + qBank.q1.qAns.wrongAns.text + "");
+    $("#ans2").val("" + qBank.q1.qAns.wrongAns.value + "");
+
+    $("#ans3").html("" + qBank.q1.qAns.correctAns.text + "");
+    $("#ans3").val("" + qBank.q1.qAns.correctAns.value + "");
+
+    $("#ans4").html("" + qBank.q1.qAns.wrongAns.text + "");
+    $("#ans4").val("" + qBank.q1.qAns.wrongAns.value + "");
+
     console.log("Cleared")
     setInterval(count, 1000);
     windowTimeout = setTimeout(function() {
@@ -62,14 +76,23 @@ function count() {
   }
 
 function ansCheck() {
-
     console.log(this);
     alert("pause");
-    reset()
+    // reset()
 }
 window.onload = function() {
-
     reset()
-    $(".buttons").on("click", ansCheck);
+    // $(".buttons").on("click", ansCheck);
+    $(".buttons button").click(function(){
+        console.log(this.id);
+        console.log($(this).attr('value'));
+        answer = $(this).attr('value')
+        if(answer == "true"){
+            alert("Correct!")
+        } else {
+            alert("Incorrect!")
+        }
+
+    })
 };
 
