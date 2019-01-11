5319a3a5276197656b55a366d7a19e4186955634
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-01-10 20:03:48 -0600

    Sloppy but function cycling of questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,137 @@
 
 var qBank = {
     q1:{
-        qText:"Question 1",
-        qAns:{
-            wrongAns:{
-                text: "wrong",
+        qText:"What is Captain Kirk's full name?",
+        qAns:{ 
+            correctAns: {
+                text: "James Tiberius Kirk",
+                value : true
+            },
+            wrongAns1:{
+                text: "James Tiger Kirk",
                 value : false
             },
-            correctAns: {
-                text: "correct",
-                value : true
+            wrongAns2:{
+                text: "William Shatner",
+                value : false
+            },
+            wrongAns3:{
+                text: "Jimmy Da Cheese",
+                value : false
             }
         }
     },
     q2:{
-        qText:"Question 2",
+        qText:"Where was Capatin Kirk born?",
         qAns:{
-            wrongAns: "wrong",
-            correctAns: "correct",
+            correctAns: {
+                text: "Riverside, Iowa",
+                value : true
+            },
+            wrongAns1:{
+                text: "Mars",
+                value : false
+            },
+            wrongAns2:{
+                text: "Las Vegas, Nevada",
+                value : false
+            },
+            wrongAns3:{
+                text: "The Bronx, New York",
+                value : false
+            }
+        }
+    },
+    q3:{
+        qText:"What was the name of Captain Kirk's vessel?",
+        qAns:{
+            correctAns: {
+                text: "USS Enterprise",
+                value : true
+            },
+            wrongAns1:{
+                text: "Death Star",
+                value : false
+            },
+            wrongAns2:{
+                text: "Deep Space Nine",
+                value : false
+            },
+            wrongAns3:{
+                text: "USS Defiant",
+                value : false
+            }
         }
     }
 
-}
+};
 
 var clockRunning;
 var random=Math.floor(Math.random() * 2);
 var correctAnswer;
 var time;
 var windowTimeout;
+var winCount;
 
-function reset() {
+function first() {
     // clearTimeout(windowTimeout);
     time = 10000;
     $("#question").text("" + qBank.q1.qText + "");
-    $("#ans1").html("" + qBank.q1.qAns.wrongAns.text + "");
-    $("#ans1").val("" + qBank.q1.qAns.wrongAns.value + "");
+    $("#ans1").html("" + qBank.q1.qAns.wrongAns1.text + "");
+    $("#ans1").val("" + qBank.q1.qAns.wrongAns1.value + "");
 
-    $("#ans2").html("" + qBank.q1.qAns.wrongAns.text + "");
-    $("#ans2").val("" + qBank.q1.qAns.wrongAns.value + "");
+    $("#ans2").html("" + qBank.q1.qAns.wrongAns2.text + "");
+    $("#ans2").val("" + qBank.q1.qAns.wrongAns2.value + "");
 
     $("#ans3").html("" + qBank.q1.qAns.correctAns.text + "");
     $("#ans3").val("" + qBank.q1.qAns.correctAns.value + "");
 
-    $("#ans4").html("" + qBank.q1.qAns.wrongAns.text + "");
-    $("#ans4").val("" + qBank.q1.qAns.wrongAns.value + "");
+    $("#ans4").html("" + qBank.q1.qAns.wrongAns3.text + "");
+    $("#ans4").val("" + qBank.q1.qAns.wrongAns3.value + "");
+
+    console.log("Cleared")
+    setInterval(count, 1000);
+    windowTimeout = setTimeout(function() {
+        alert("Times Up!");
+        }, time);
+  }
+
+  function second(){
+    time = 10000;
+    $("#question").text("" + qBank.q2.qText + "");
+    $("#ans1").html("" + qBank.q2.qAns.wrongAns1.text + "");
+    $("#ans1").val("" + qBank.q2.qAns.wrongAns1.value + "");
+
+    $("#ans2").html("" + qBank.q2.qAns.correctAns.text + "");
+    $("#ans2").val("" + qBank.q2.qAns.correctAns.value + "");
+
+    $("#ans3").html("" + qBank.q2.qAns.wrongAns2.text + "");
+    $("#ans3").val("" + qBank.q2.qAns.wrongAns2.value + "");
+
+    $("#ans4").html("" + qBank.q2.qAns.wrongAns3.text + "");
+    $("#ans4").val("" + qBank.q2.qAns.wrongAns3.value + "");
+
+    console.log("Cleared")
+    setInterval(count, 1000);
+    windowTimeout = setTimeout(function() {
+        alert("Times Up!");
+        }, time);
+  }
+
+  function third(){
+    time = 10000;
+    $("#question").text("" + qBank.q3.qText + "");
+    $("#ans1").html("" + qBank.q3.qAns.correctAns.text + "");
+    $("#ans1").val("" + qBank.q3.qAns.correctAns.value + "");
+
+    $("#ans2").html("" + qBank.q3.qAns.wrongAns1.text + "");
+    $("#ans2").val("" + qBank.q3.qAns.wrongAns1.value + "");
+
+    $("#ans3").html("" + qBank.q3.qAns.wrongAns2.text + "");
+    $("#ans3").val("" + qBank.q3.qAns.wrongAns2.value + "");
+
+    $("#ans4").html("" + qBank.q3.qAns.wrongAns3.text + "");
+    $("#ans4").val("" + qBank.q3.qAns.wrongAns3.value + "");
 
     console.log("Cleared")
     setInterval(count, 1000);
@@ -53,14 +141,10 @@ function reset() {
   }
 
 
-
 function count() {
 
-    // DONE: increment time by 1, remember we cant use "this" here.
     time=time-1000;
     console.log(time)
-    // DONE: Get the current time, pass that into the timeConverter function,
-    //       and save the result in a variable.
     var converted = timeConverter(time);
     
     if (time>=0) {
@@ -75,24 +159,51 @@ function count() {
     return "Seconds Remaining " + seconds;
   }
 
-function ansCheck() {
-    console.log(this);
-    alert("pause");
-    // reset()
-}
-window.onload = function() {
-    reset()
-    // $(".buttons").on("click", ansCheck);
-    $(".buttons button").click(function(){
+  function play(){
+      first()
+      $(".buttons button").click(function(){
+        clearTimeout(windowTimeout)
         console.log(this.id);
         console.log($(this).attr('value'));
         answer = $(this).attr('value')
         if(answer == "true"){
             alert("Correct!")
+            winCount++;
         } else {
             alert("Incorrect!")
         }
+        second()
+        $(".buttons button").click(function(){
+            clearTimeout(windowTimeout)
+            console.log(this.id);
+            console.log($(this).attr('value'));
+            answer = $(this).attr('value')
+            if(answer == "true"){
+                alert("Correct!")
+                winCount++;
+            } else {
+                alert("Incorrect!")
+            }
+            third()
+            $(".buttons button").click(function(){
+                clearTimeout(windowTimeout)
+                console.log(this.id);
+                console.log($(this).attr('value'));
+                answer = $(this).attr('value')
+                if(answer == "true"){
+                    alert("Correct!")
+                    winCount++;
+                } else {
+                    alert("Incorrect!")
+                }   
 
-    })
-};
+                })
+            })
+        })
+    };
+
+window.onload = function() {
+    play()
+
+    };
 
